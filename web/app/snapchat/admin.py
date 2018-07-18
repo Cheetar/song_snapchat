1130753b6677b6ce0f7e3432778e30df23beb2db
Cheetar <mateusz@sieniawski.net>
2018-07-18 18:54:20 +0200

    Added unlisten feature in admin panel

--- web/app/snapchat/admin.py
+++ web/app/snapchat/admin.py
@@ -3,16 +3,40 @@ from django.contrib import admin
 from .models import Snap, Song
 
 
+def unlisten(songs):
+    """ Makes all songs from queryset songs unvisited and sets their
+        listened_on field to None
+    """
+    songs.update(visited=False)
+    songs.update(listened_on=None)
+
+
+def unlistenSong(modeladmin, request, queryset):
+    unlisten(queryset)
+
+
+def unlistenSnap(modeladmin, request, queryset):
+    for snap in queryset:
+        songs = Song.objects.filter(snap=snap)
+        unlisten(songs)
+
+
+unlistenSong.short_description = "Unlisten selected songs"
+unlistenSnap.short_description = "Unlisten all songs from seleceted snaps"
+
+
 class SongAdmin(admin.ModelAdmin):
     list_display = ('name', 'snap', 'created_on', 'listened_on', 'upload')
-    list_filter = ('visited',)
+    list_filter = ('visited', 'snap')
     search_fields = ('name', 'description')
     ordering = ('-listened_on',)
+    actions = [unlistenSong]
 
 
 class SnapAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_on')
     ordering = ('-created_on',)
+    actions = [unlistenSnap]
 
 
 admin.site.register(Snap, SnapAdmin)
--- web/app/snapchat/models.py
+++ web/app/snapchat/models.py
@@ -8,13 +8,6 @@ def generate_token():
     return binascii.hexlify(os.urandom(16)).decode()
 
 
-def restart_songs():
-    for song in Song.objects.all():
-        song.visited = False
-        song.listened_on = None
-        song.save()
-
-
 class Snap(models.Model):
     id = models.AutoField(primary_key=True)
     token = models.CharField(max_length=32, blank=True)
